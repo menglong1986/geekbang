6faf61a6d921494f3fd1a1e3c0d4861439bbc1bd
random <txbhcml@163.com>
2021-12-22 16:37:09 +0800

    第二题

---
+++ 第五周/2/User.java
@@ -0,0 +1,31 @@
+package com.example.week5.two;
+
+public class User {
+
+    private String username;
+    private Integer password;
+
+    public User() {
+    }
+
+    public User(String username, Integer password ){
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getPassword() {
+        return password;
+    }
+
+    public void setPassword(Integer password) {
+        this.password = password;
+    }
+}
---
+++ 第五周/2/UserDao.java
@@ -0,0 +1,13 @@
+package com.example.week5.two;
+
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDao {
+
+    public void sava() {
+        System.out.println(" save ....");
+    }
+
+}
